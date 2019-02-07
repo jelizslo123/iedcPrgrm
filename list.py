64391d7effcf396ed4e0b4e0843006e64588eea3
jelizslo123 <slomojelizeba@gmail.com>
2019-02-07 09:58:27 +0530

    Subjects

---
+++ list.py
@@ -0,0 +1,8 @@
+subjects=["Physics","Chemistry","Biology","Maths","English"]
+print(subjects)
+print(subjects[0])
+print(subjects[0:3])
+subjects[0]= "Malayalam"
+print(subjects)
+del(subjects[1])
+print(subjects)
